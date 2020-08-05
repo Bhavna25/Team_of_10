2fc1387b08db6f03c9e7fe0d8a18a471d1e27347
Bhavna25 <63115179+Bhavna25@users.noreply.github.com>
2020-08-05 19:09:12 +0530

    Create Russian_Peasant.c
    Day 12:
    Problem 1:
    Hey, Have you heard of russian peasant algorithm? No, it's okay. Just think of how will you be able to multiply two numbers using bitwise operations. Yeah, you can't use this "*" operator to multiply. Can you think of it? Just try it out.
    
    Problem 2:
    This problem is easy one, once you solve it, you can try questions in the backlog.
    
    Given an unsigned integer N. The task is to swap all odd bits with even bits. For example, if the given number is 23 (00010111), it should be converted to 43(00101011). Here, every even position bit is swapped with adjacent bit on right side(even position bits are highlighted in binary representation of 23), and every odd position bit is swapped with adjacent on left side.
    
    Input:
    The first line of input contains T, denoting the number of testcases. Each testcase contains single line.
    
    Output:
    For each testcase in new line, print the converted number.
    
    Constraints:
    1 ≤ T ≤ 100
    1 ≤ N ≤ 100
    
    Example:
    Input:
    2
    23
    2
    
    Output:
    43
    1
    
    Explanation:
    Testcase 1: BInary representation of the given number; 00010111 after swapping 00101011.

---
+++ Russian_Peasant.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,res=0;
+    scanf("%d %d",&a,&b);
+    while(b!=0)
+    {
+        if(b&1)
+        {
+            res=res+a;
+        }
+        a=a<<1;
+        b=b>>1;
+    }
+    printf("%d",res);
+    return 0;
+}
