b59b8b4094e29467ea343d97392f7a02c4bbd1cb
Bhavna25 <63115179+Bhavna25@users.noreply.github.com>
2020-08-05 19:01:03 +0530

    Create LongestSubstring.cpp
    Day 19:
    Problem 1:
    Given a string, find the length of the longest sub-string without repeating characters. For example, the longest sub-strings without repeating characters for “ABDEFGABEF” are “BDEFGA” and “DEFGAB”, with length 6.
    
    Example 1:
    
    Input: "abcabcbb"
    Output: 3 
    Explanation: The answer is "abc", with the length of 3. 
    Example 2:
    
    Input: "bbbbb"
    Output: 1
    Explanation: The answer is "b", with the length of 1.
    Example 3:
    
    Input: "pwwkew"
    Output: 3
    Explanation: The answer is "wke", with the length of 3. 
                 Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
    
    This problem is quite popular but solving it with brute force can be easy, but try with sliding window approach to reduce the complexity.

---
+++ LongestSubstring.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int longestUniqueSubsttr(string str) 
+{ 
+	int n = str.size(); 
+	int res = 0; 
+	for (int i = 0; i < n; i++) {  
+		vector<bool> visited(256); 
+
+		for (int j = i; j < n; j++) {  
+			if (visited[str[j]] == true) 
+				break; 
+			else { 
+				res = max(res, j - i + 1); 
+				visited[str[j]] = true; 
+			} 
+		} 
+		visited[str[i]] = false; 
+	} 
+	return res; 
+} 
+int main() 
+{ 
+	string str; 
+    cin>>str;
+	int len = longestUniqueSubsttr(str); 
+	cout <<len; 
+	return 0; 
+} 
